dc9f576d99816bb9bb527061d4b0fd5706c1b200
Marcos Roberto <marcosroberto1808@gmail.com>
2018-09-26 20:51:13 -0300

    update projects

---
+++ docker/databases/postgres-example/sql_scripts/01-createdb_sic.sql
@@ -0,0 +1,11 @@
+-- Database: sic
+
+-- DROP DATABASE sic;
+
+CREATE DATABASE sic
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'pt_BR.UTF-8'
+       LC_CTYPE = 'pt_BR.UTF-8'
+       CONNECTION LIMIT = -1;
---
+++ docker/databases/postgres-example/sql_scripts/02-createdb_db_portal_digital.sql
@@ -0,0 +1,11 @@
+-- Database: db_portal_digital
+
+-- DROP DATABASE db_portal_digital;
+
+CREATE DATABASE db_portal_digital
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'pt_BR.UTF-8'
+       LC_CTYPE = 'pt_BR.UTF-8'
+       CONNECTION LIMIT = -1;
